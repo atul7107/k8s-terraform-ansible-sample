785cadafa2dab9a8136c2114fcef004f9830c283
atul7107 <atul.yadav7717@gmail.com>
2019-02-12 16:30:28 +0530

    Update aws.tf

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,6 +1,6 @@
 # Retrieve AWS credentials from env variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
 provider "aws" {
-  access_key = ""
-  secret_key = ""
+  access_key = "AKIAJWO7RVNF2MZ7BYJQ"
+  secret_key = "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"
   region = "${var.region}"
 }
